47d8edd68d7d0e89c8c52d4d55a46a0a71951061
eci4ever <eci4ever@gmail.com>
2025-09-12 10:41:37 +0800

    Add user info on page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,17 +5,28 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 import HeaderBar from "./ui/header"
+import { useUser } from "./user-context"
 
 export default function Page() {
+
+  const user = useUser()
+
   return (
 
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl">My Roles : {user?.roles}</div>
+        <div className="bg-muted/50 aspect-video rounded-xl"></div>
         <div className="bg-muted/50 aspect-video rounded-xl" />
       </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+        My Permissions :
+        <ul>
+          {user?.permissions.map((key, perm) => (
+            <li>{key} {perm} </li>
+          ))}
+        </ul>
+      </div>
     </div>
 
   )
