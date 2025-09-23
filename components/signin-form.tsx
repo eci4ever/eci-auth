731ec1cc28086d3849c6ed743292b8bcbc3bef3e
eci4ever <eci4ever@gmail.com>
2025-09-23 11:18:52 +0800

    remove link for password reset

--- components/signin-form.tsx
+++ components/signin-form.tsx
@@ -119,7 +119,7 @@ export function SignInForm() {
                         <div className="space-y-2">
                             <div className="flex items-center justify-between">
                                 <Label htmlFor="password">Password</Label>
-                                <Link href="/forgot-password" className="text-sm text-primary hover:underline">
+                                <Link href="#" className="text-sm text-primary hover:underline">
                                     Forgot password?
                                 </Link>
                             </div>
